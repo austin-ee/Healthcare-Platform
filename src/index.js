0364801f7fb0607a5c04a8d55a6bc2263bbe7069
aust.inee <onyangoaustine643@gmail.com>
2025-07-15 13:11:21 +0300

    configuration change

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import './styles.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'react-toastify/dist/ReactToastify.css'
-import {ToastContainer}from 'react-toastify';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
